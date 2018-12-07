b0ef0787ed125759fbf0df7a8e7e91d669193bc2
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-12-07 15:20:54 -0500

    blocked out comments for demo

---
+++ script.js
@@ -0,0 +1,45 @@
+// THE DOM
+// Declare a new variable that will hold the page title
+
+// Grab a reference to the #page-title element
+
+// Set the innerHTML of the page title
+
+// Build an object representing the current weather conditions
+
+// Get a reference to the DOM elements
+
+// Set their innerHTML to the corresponding data
+
+// Build another weather object representing tomorrow's weather
+// Include an icon url: https://cdn1.iconfinder.com/data/icons/weather-elements/512/Weather_SunAbstract.png
+
+
+// Build an HTML string to display the weather information (with an img)
+
+// Get a reference to the #tomorrow section and set its innerHTML to the HTML string
+
+
+
+
+//CONDITIONALS
+
+// 2+2 === 4 --> evaluates to true 
+// If statements check to see if the code in the parentheses evaluates to true
+
+
+// Check and see if a user is logged in
+// If they are, print "Welcome back!""
+// If they're not, print "Please Log In!""
+
+
+// Declare a variable that represents a time of day
+
+// Check to see if the time of day is "morning", "afternoon", etc and print a greeting accordingly into "conditionals-container"
+
+// Declare a variable that represents the current temperature
+// Check to see if the temperature is less than or equal to 32. How about greater than or equal to 85? 
+
+
+
+
