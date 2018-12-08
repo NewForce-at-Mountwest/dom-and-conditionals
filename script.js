ac67468269db4011f20a41c43445c87df65e133c
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-12-08 11:14:21 -0500

    added demo code for the dom chapter

--- script.js
+++ script.js
@@ -1,23 +1,58 @@
 // THE DOM
-// Declare a new variable that will hold the page title
 
 // Grab a reference to the #page-title element
+var headingContainer = document.querySelector("#page-title");
 
 // Set the innerHTML of the page title
+headingContainer.innerHTML = "This is my awesome website!";
+console.log(headingContainer);
+
 
 // Build an object representing the current weather conditions
-
+var todaysWeather = {
+    temperature: 32,
+    date: "Saturday, Dec 8",
+    conditions: "Sunny and cold"
+}
 // Get a reference to the DOM elements
+var tempContainer = document.querySelector("#temperature");
+var dateContainer = document.querySelector("#todays-date");
+var conditionsContainer = document.querySelector("#conditions");
 
 // Set their innerHTML to the corresponding data
+tempContainer.innerHTML = todaysWeather.temperature;
+dateContainer.innerHTML = todaysWeather.date;
+conditionsContainer.innerHTML = todaysWeather.conditions;
+
 
 // Build another weather object representing tomorrow's weather
 // Include an icon url: https://cdn1.iconfinder.com/data/icons/weather-elements/512/Weather_SunAbstract.png
-
+var tomorrowWeather = {
+    temperature: 75,
+    date: "Sunday, Dec 9",
+    conditions: "Sunny",
+    imageUrl: "https://cdn1.iconfinder.com/data/icons/weather-elements/512/Weather_SunAbstract.png"
+}
 
 // Build an HTML string to display the weather information (with an img)
+var HTMLString = `
+    <h3>${tomorrowWeather.date}</h3>
+    <p>${tomorrowWeather.temperature}</p>
+    <p>${tomorrowWeather.conditions}</p>
+    <img class="weather-icon" src=${tomorrowWeather.imageUrl} alt="Weather icon"/>`
 
-// Get a reference to the #tomorrow section and set its innerHTML to the HTML string
+
+    // Get a reference to the #tomorrow section and set its innerHTML to the HTML string
+var tomorrowContainer = document.querySelector("#tomorrow");
+tomorrowContainer.innerHTML = HTMLString;
+
+// This does the same thing:
+// document.querySelector("#tomorrow").innerHTML = HTMLString;
+
+
+
+
+
 
 
 
